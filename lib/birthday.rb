8c16f17380071ce49791888196a714a78d360604
Christopher Dent <christopherdent01@gmail.com>
2019-08-01 00:54:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,7 +10,7 @@ birthday_kids.each do |name, age|
  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 
 end
-
+happy_birthday(birthday_kids)
 end
 
-happy_birthday(birthday_kids)
+
